6889784619cbff96c5f2af3a63ac11ea74e3e341
devanfedhi <devanfedhi1@gmail.com>
2025-02-09 18:20:28 +1100

    test

---
+++ blazor-app/blazor-app/Program.cs
@@ -0,0 +1,34 @@
+using blazor_app.Client.Pages;
+using blazor_app.Components;
+using blazor_app.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapHub<ChatHub>("/chathub");
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(blazor_app.Client._Imports).Assembly);
+
+app.Run("http://*:5000");
